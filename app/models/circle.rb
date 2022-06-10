964fec94a03555886b9d50256a29b76c98bfb1d8
pumpkin-tart <horie.wbtc@gmail.com>
2022-06-10 12:51:25 +0900

    Add: ファイルの追加

--- app/models/circle.rb
+++ app/models/circle.rb
@@ -2,6 +2,7 @@ class Circle < ApplicationRecord
   mount_uploader :circle_image, CircleImageUploader
   belongs_to :event
   has_many :items, dependent: :destroy
+  accepts_nested_attributes_for :items
 
   validates :circle_name, presence: true, length: { maximum: 255 }
 end
