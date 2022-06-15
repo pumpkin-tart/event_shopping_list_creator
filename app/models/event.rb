18996c61e90d5f8a8e1193a38d442f156d61401a
pumpkin-tart <horie.wbtc@gmail.com>
2022-06-15 13:19:00 +0900

    Add: パスワードリセット機能の追加

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,6 +2,7 @@ class Event < ApplicationRecord
   mount_uploader :map_image, MapImageUploader
   belongs_to :user
   has_many :circles, dependent: :destroy
+  has_many :items, through: :circles
   accepts_nested_attributes_for :circles
 
   validates :title, presence: true, length: { maximum: 255 }
