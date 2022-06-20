352d1638ef057db8db3e1ad4ea8abce17ac7f636
pumpkin-tart <horie.wbtc@gmail.com>
2022-06-20 20:05:19 +0900

    Add: パスワードリセット

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,10 +1,11 @@
 class ItemsController < ApplicationController
 
   def create
+    @event = Event.find(params[:event_id])
     @circle = Circle.find(params[:circle_id])
     @item = @circle.items.build(item_params)
     if @item.save
-      redirect_to event_circle_path(@item.id), success: t('defaults.message.created', item: 'アイテム')
+      redirect_to event_circle_path(@event, @circle, @item), success: t('defaults.message.created', item: 'アイテム')
     else
       flash.now['danger'] = t('defaults.message.not_created', item: 'アイテム')
       render :new
@@ -12,25 +13,29 @@ class ItemsController < ApplicationController
   end
 
   def show
+    @event = Event.find(params[:event_id])
     @circle = Circle.find(params[:circle_id])
     @item = @circle.items.find(params[:id])
   end
 
   def new
+    @event = Event.find(params[:event_id])
     @circle = Circle.find(params[:circle_id])
     @item = @circle.items.build
   end
 
   def edit
+    @event = Event.find(params[:event_id])
     @circle = Circle.find(params[:circle_id])
-    @item = current_user.items.find(params[:id])
+    @item = @circle.items.find(params[:id])
   end
 
   def destroy
+    @event = Event.find(params[:event_id])
     @circle = Circle.find(params[:circle_id])
     @item = Item.find(params[:id])
     @item.destroy!
-    redirect_to @circle, success: t('defaults.message.deleted', item: 'イベントMAP')
+    redirect_to event_circle_path(@event, @circle), success: t('defaults.message.deleted', item: 'アイテム')
   end
 
   private
--- app/uploaders/circle_image_uploader.rb
+++ app/uploaders/circle_image_uploader.rb
@@ -35,13 +35,19 @@ class CircleImageUploader < CarrierWave::Uploader::Base
 
   # Add an allowlist of extensions which are allowed to be uploaded.
   # For images you might use something like this:
-  # def extension_allowlist
-  #   %w(jpg jpeg gif png)
-  # end
+  def extension_allowlist
+    %w(jpg jpeg gif png)
+  end
 
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
   # def filename
   #   "something.jpg" if original_filename
   # end
+
+  def default_url
+    'sample.jpg'
+  end
+
+
 end
