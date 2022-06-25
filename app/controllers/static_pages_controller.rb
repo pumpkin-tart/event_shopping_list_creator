83f6f29ef3de8cb7155d25324f67843c14fb7d43
pumpkin-tart <horie.wbtc@gmail.com>
2022-06-25 13:33:37 +0900

    Add: トップページを作成

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,7 @@
 class StaticPagesController < ApplicationController
-    skip_before_action :require_login, only: %i[top]
+  skip_before_action :require_login, only: %i[top]
 
-    def top
-        redirect_to login_path
-    end
+  def top
+    render "static_pages/top"
+  end
 end
