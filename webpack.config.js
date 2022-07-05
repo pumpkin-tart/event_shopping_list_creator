964215c89cb7ffad4d26b0eb43459bd8baf73f8b
pumpkin-tart <horie.wbtc@gmail.com>
2022-07-05 22:25:43 +0900

    Add

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,20 @@
 const path = require('path')
-const webpack = require('webpack')
 
 module.exports = {
-  entry: {
-    index: path.join(__dirname, 'src', 'index.js')
-  },
+  // 作業ディレクトリがある場合はentryの書き方が変わる
+  context: `${__dirname}/src`,
+  // entry: './index.js',
+  // joinを使用するとOSの環境に合わせてパスをいい感じに設定してくれる。
+  entry: path.join(__dirname, '/src', 'index.js'),
+
+  // defaultの設定つまりあってもなくても一緒
+  // ファイルの起点を設定する。
+  // entry: './src/index.js',
+
+  // ここは絶対パスで指定する必要がある。
   output: {
-    path: path.join(__dirname, 'out'),
+    // path: `${__dirname}/dist`,
+    path: path.join(__dirname, '/dist'),
     filename: 'main.js'
-  },
-  devtool: 'cheap-module-eval-source-map',
-  target: 'node',
-  module: {
-    rules: [
-      {
-        test: /.js$/,
-        loader: 'babel-loader'
-      }
-    ]
-  },
-  plugins: [
-    externalPlugins
-  ]
+  }
 }
