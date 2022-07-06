72c526ca4b897a75b91fffb385ae7e1b6118071a
pumpkin-tart <horie.wbtc@gmail.com>
2022-07-06 21:45:09 +0900

    Add

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const http = require('http');
 const hostname = '127.0.0.1';
 const port = process.env.PORT || 3000;
 
+
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
@@ -15,3 +16,7 @@ server.on('listening', () => {
   const port = server.address().port;
 });
 
+server.listen(process.env.PORT || 8000, "0.0.0.0", (err, address) => {
+    if (err) throw err
+    
+})
