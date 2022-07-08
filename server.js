2300a4ec6abbb9575db2e5aa866f03616993404c
pumpkin-tart <horie.wbtc@gmail.com>
2022-07-08 18:26:12 +0900

    Add

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const port = process.env.PORT || 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end();
+
 });
 
 
